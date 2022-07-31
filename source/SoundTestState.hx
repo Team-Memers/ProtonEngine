a2c898f637e1366bfcec64e808af47131e411670
ThePootisBird <30559271+ThePootisBird@users.noreply.github.com>
2022-07-31 12:29:57 -0400

    sound test

--- source/SoundTestState.hx
+++ source/SoundTestState.hx
@@ -13,472 +13,146 @@ import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import lime.utils.Assets;
+import hscript.Interp;
+import hscript.Parser;
+import hscript.ParserEx;
+import hscript.InterpEx;
+import sys.FileSystem;
+import Song.SwagSong;
 
 #if windows
 import Discord.DiscordClient;
 #end
 
 class SoundTestState extends MusicBeatState
-{
-	var woahmanstopspammin:Bool = true;
+{	
+	var hscriptStates:Map<String, Interp> = [];
+	var exInterp:InterpEx = new InterpEx();
+	var haxeSprites:Map<String, FlxSprite> = [];
 
-	var whiteshit:FlxSprite;
-
-	var daValue:Int = 0;
-	var pcmValue:Int = 0;
-
-	var soundCooldown:Bool = true;
-
-	var funnymonke:Bool = true;
-
-	var incameo:Bool = false;
-
-	var cameoBg:FlxSprite;
-	var cameoImg:FlxSprite;
-	var cameoThanks:FlxSprite;
-
-	var pcmNO = new FlxText(FlxG.width / 6, FlxG.height / 2, 0, 'PCM  NO .', 23);
-	var daNO = new FlxText(FlxG.width * .6, FlxG.height / 2, 0, 'DA  NO .', 23);
-
-	var pcmNO_NUMBER = new FlxText(FlxG.width / 6, FlxG.height / 2, 0, '0', 23);
-	var daNO_NUMBER = new FlxText(FlxG.width / 6, FlxG.height / 2, 0, '0', 23);
-	
-
-    override function create()
-        {
-
-			new FlxTimer().start(0.1, function(tmr:FlxTimer)
-				{
-				});
-		
-			whiteshit = new FlxSprite().makeGraphic(1280, 720, FlxColor.WHITE);
-			whiteshit.alpha = 0;
-
-			cameoBg = new FlxSprite();
-			cameoImg = new FlxSprite();
-			cameoThanks = new FlxSprite();
-
-			FlxG.sound.music.stop();
-
-			var bg:FlxSprite = new FlxSprite(-100).loadGraphic(Paths.image('menuBG'));
-			bg.scrollFactor.x = 0;
-			bg.scrollFactor.y = 0;
-			bg.setGraphicSize(Std.int(bg.width * 1));
-			bg.updateHitbox();
-			bg.screenCenter();
-			bg.antialiasing = true;
-			add(bg);
-
-			var soundtesttext = new FlxText(0, 0, 0, 'assets/fonts/vcr.ttf', 25);
-			soundtesttext.setFormat("assets/fonts/vcr.ttf", 23, FlxColor.fromRGB(255, 255, 255));
-                        soundtesttext.text = "SOUND TEST";
-			soundtesttext.screenCenter();
-			soundtesttext.y -= 180;
-			soundtesttext.x -= 33;
-			soundtesttext.setFormat("vcr", 25, FlxColor.fromRGB(0, 163, 255));
-			soundtesttext.setBorderStyle(SHADOW, FlxColor.BLACK, 4, 1);
-			add(soundtesttext);
-			
-
-
-			pcmNO.setFormat("assets/fonts/vcr.ttf", 23, FlxColor.fromRGB(174, 179, 251));
-			pcmNO.setBorderStyle(SHADOW, FlxColor.fromRGB(106, 110, 159), 4, 1);
-
-			daNO.setFormat("assets/fonts/vcr.ttf", 23, FlxColor.fromRGB(174, 179, 251));
-			daNO.setBorderStyle(SHADOW, FlxColor.fromRGB(106, 110, 159), 4, 1);
-
-
-
-			
-			pcmNO.y -= 70;
-			pcmNO.x += 100;
-
-			daNO.y -= 70;
-			
-			add(pcmNO);
-
-			add(daNO);
-
-			
-			pcmNO_NUMBER.y -= 70;
-			pcmNO_NUMBER.x += 270;
-			pcmNO_NUMBER.setFormat("funkin", 23, FlxColor.fromRGB(174, 179, 251));
-			pcmNO_NUMBER.setBorderStyle(SHADOW, FlxColor.fromRGB(106, 110, 159), 4, 1);
-			add(pcmNO_NUMBER);
-
-			
-			daNO_NUMBER.y -= 70;
-			daNO_NUMBER.x += daNO.x - 70;
-			daNO_NUMBER.setFormat("funkin", 23, FlxColor.fromRGB(174, 179, 251));
-			daNO_NUMBER.setBorderStyle(SHADOW, FlxColor.fromRGB(106, 110, 159), 4, 1);
-			add(daNO_NUMBER);
-
-			cameoBg.visible = false;
-			add(cameoBg);
-
-			cameoThanks.visible = false;
-			add(cameoThanks);
-
-			cameoImg.visible = false;
-			add(cameoImg);
-
-
-
-			add(whiteshit);
-
-			
-        }
-
-	function changeNumber(selection:Int) 
-	{
-		if (funnymonke)
-		{
-			pcmValue += selection;
-			if (pcmValue < 0) pcmValue = 99;
-			if (pcmValue > 99) pcmValue = 0;
+	function callHscript(func_name:String, args:Array<Dynamic>, usehaxe:String) {
+		// if function doesn't exist
+		if (!hscriptStates.get(usehaxe).variables.exists(func_name)) {
+			trace("Function doesn't exist, silently skipping...");
+			return;
 		}
-		else
-		{
-			daValue += selection;
-			if (daValue < 0) daValue = 99;
-			if (daValue > 99) daValue = 0;
+		var method = hscriptStates.get(usehaxe).variables.get(func_name);
+		switch(args.length) {
+			case 0:
+				method();
+			case 1:
+				method(args[0]);
+			case 2:
+				method(args[0], args[1]);
+			case 3:
+				method(args[0], args[1], args[2]);
+			case 4:
+				method(args[0], args[1], args[2], args[3]);
+			case 5:
+				method(args[0], args[1], args[2], args[3], args[4]);
 		}
 	}
-
-	function flashyWashy(a:Bool)
-	{
-		if (a == true)
-		{
-			FlxG.sound.play(Paths.sound('confirmMenu'));
-			FlxTween.tween(whiteshit, {alpha: 1}, 0.4);
+	function callAllHScript(func_name:String, args:Array<Dynamic>) {
+		for (key in hscriptStates.keys()) {
+			callHscript(func_name, args, key);
 		}
-		else
-			FlxTween.color(whiteshit, 0.1, FlxColor.WHITE, FlxColor.BLUE);
-			FlxTween.tween(whiteshit, {alpha: 0}, 0.2);
+	}
+	function setHaxeVar(name:String, value:Dynamic, usehaxe:String) {
+		hscriptStates.get(usehaxe).variables.set(name,value);
+	}
+	function getHaxeVar(name:String, usehaxe:String):Dynamic {
+		return hscriptStates.get(usehaxe).variables.get(name);
+	}
+	function setAllHaxeVar(name:String, value:Dynamic) {
+		for (key in hscriptStates.keys())
+			setHaxeVar(name, value, key);
+	}
+	function makeHaxeState(usehaxe:String, path:String, filename:String) {
+		trace("opening a haxe state (because we are cool :))");
+		var parser = new ParserEx();
+		var program = parser.parseString(FNFAssets.getHscript(path + filename));
+		var interp = PluginManager.createSimpleInterp();
+		// set vars
+		interp.variables.set("Sys", Sys);
+		interp.variables.set("FlxTextBorderStyle", FlxTextBorderStyle);
+		interp.variables.set("controls", controls);
+		interp.variables.set("FlxTransitionableState", FlxTransitionableState);
+		interp.variables.set("MainMenuState", MainMenuState);
+		interp.variables.set("CategoryState", CategoryState);
+		interp.variables.set("ChartingState", ChartingState);
+		interp.variables.set("Alphabet", Alphabet);
+		interp.variables.set("curBeat", 0);
+		interp.variables.set("currentMainMenuState", this);
+		interp.variables.set("add", add);
+		interp.variables.set("remove", remove);
+		interp.variables.set("insert", insert);
+		interp.variables.set("pi", Math.PI);
+		interp.variables.set("curMusicName", Main.curMusicName);
+		interp.variables.set("StoryMenuState", StoryMenuState);
+		interp.variables.set("FreeplayState", FreeplayState);
+		interp.variables.set("CreditsState", CreditsState);
+		interp.variables.set("SaveDataState", SaveDataState);
+		interp.variables.set("togglePersistUpdate", togglePersistUpdate);
+		interp.variables.set("togglePersistDraw", togglePersistDraw);
+		interp.variables.set("coolURL", coolURL);
+		interp.variables.set("flixelSave", FlxG.save);
+		interp.variables.set("FlxTween", FlxTween);	
+		interp.variables.set("FlxText", FlxText);
+		interp.variables.set("FlxMath", FlxMath);
+		interp.variables.set("FlxG", FlxG);	
+		interp.variables.set("PlayStateChangeables", PlayStateChangeables);	
+		interp.variables.set("PlayState", PlayState);	
 
+		trace("set stuff");
+		interp.execute(program);
+		hscriptStates.set(usehaxe,interp);
+		callHscript("create", [], usehaxe);
+		trace('executed');
 	}
 
-	function doTheThing(first:Int, second:Int) 
+	function togglePersistUpdate(toggle:Bool)
 	{
-		if (first == 12 && second == 25)
-		{
-			woahmanstopspammin = false;
-			PlayStateChangeables.nocheese = false;
-			PlayState.SONG = Song.loadFromJson('endless-hard', 'endless');
-			PlayState.isStoryMode = false;
-			PlayState.storyDifficulty = 2;
-			PlayState.storyWeek = 1;
-			FlxTransitionableState.skipNextTransIn = true;
-			FlxTransitionableState.skipNextTransOut = true;
-			flashyWashy(true);
-			new FlxTimer().start(2, function(tmr:FlxTimer)
-			{
-				LoadingState.loadAndSwitchState(new PlayState());
-			});
-		}
-		else if (first == 7 && second == 7)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('cycles-hard', 'cycles');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 31 && second == 13)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				StoryMenuState.storySongPlaylist = ['faker', 'black-sun'];
-				PlayState.SONG = Song.loadFromJson('faker-hard', 'faker');
-				PlayState.isStoryMode = true;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 66 && second == 6)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('sunshine-hard', 'sunshine');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 8 && second == 21)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('chaos-hard', 'chaos');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 0 && second == 0)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('too-fest-hard', 'too-fest');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 8 && second == 10)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('hill-of-the-void-hard', 'hill-of-the-void');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		       else if (first == 1 && second == 1)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('fate-hard', 'fate');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 69 && second == 69)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('thigh-highs-hard', 'thigh-highs');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 86 && second == 19)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('blood-red-snow-hard', 'blood-red-snow');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 24 && second == 13)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('slaybells-hard', 'slaybells');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 99 && second == 9)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('fatality-hard', 'fatality');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 60 && second == 6)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('prey-hard', 'prey');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 1 && second == 2)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('fight-or-flight-hard', 'fight-or-flight');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 33 && second == 3)
-			{
-				woahmanstopspammin = false;
-				PlayStateChangeables.nocheese = false;
-				PlayState.SONG = Song.loadFromJson('round-a-bout-hard', 'round-a-bout');
-				PlayState.isStoryMode = false;
-				PlayState.storyDifficulty = 2;
-				PlayState.storyWeek = 1;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					LoadingState.loadAndSwitchState(new PlayState());
-				});
-			}
-		else if (first == 69 && second == 72) // sopping wet
-			{
-				woahmanstopspammin = false;
-				flashyWashy(true);
-				new FlxTimer().start(2, function(tmr:FlxTimer)
-				{
-					cameoImg.visible = true;
-					cameoImg.loadGraphic("assets/images/i am wet.jpg");
-					cameoImg.setGraphicSize(FlxG.width, FlxG.height);
-					flashyWashy(false);
-					FlxG.sound.music.stop();
-	
-				});
-				new FlxTimer().start(2.1, function(tmr:FlxTimer)
-				{
-					incameo = true;
-				});
-			}
-		else
-		{
-			if (soundCooldown)
-			{
-				soundCooldown = false;
-				FlxG.sound.play(Paths.sound('deniedMOMENT'));
-				new FlxTimer().start(0.8, function(tmr:FlxTimer)
-				{
-					soundCooldown = true;
-				});
-			}
-                }
+		persistentUpdate = toggle;
 	}
+
+	function togglePersistDraw(toggle:Bool)
+	{
+		persistentDraw = toggle;
+	}
+
+	function coolURL(url:String):String
+	{
+		FlxG.openURL(url);
+		return url;
+	}
+
+	override function create()
+	{
+		FNFAssets.clearStoredMemory();
 		
-	override public function update(elapsed:Float)
-		{
-			if (FlxG.keys.justPressed.RIGHT || FlxG.keys.justPressed.LEFT || FlxG.keys.justPressed.A || FlxG.keys.justPressed.D) if (woahmanstopspammin) funnymonke = !funnymonke;
+		#if windows
+		// Updating Discord Rich Presence
+		var customPrecence = TitleState.discordStuff.mainmenu;
+		Discord.DiscordClient.changePresence(customPrecence, null);
+		#end
 
-			if (FlxG.keys.justPressed.DOWN || FlxG.keys.justPressed.S) if (woahmanstopspammin) changeNumber(1);
+		makeHaxeState("soundtest", "assets/scripts/custom_menus/", "SoundTestState");
 
-			if (FlxG.keys.justPressed.UP || FlxG.keys.justPressed.W) if (woahmanstopspammin) changeNumber(-1);
+		super.create();
+	}
 
-			if (FlxG.keys.justPressed.ENTER && woahmanstopspammin) doTheThing(pcmValue, daValue);
+	override function update(elapsed:Float)
+	{
+		callAllHScript("update", [elapsed]);
+		super.update(elapsed);
+	}
 
-			if (FlxG.keys.justPressed.ENTER && !woahmanstopspammin && incameo) LoadingState.loadAndSwitchState(new SoundTestState());
+	override function beatHit()
+	{
+		super.beatHit();
+		setAllHaxeVar('curBeat', curBeat);
 
-			if (FlxG.keys.justPressed.ESCAPE && woahmanstopspammin && !incameo) LoadingState.loadAndSwitchState(new MainMenuState());
-
-			if (funnymonke)
-			{
-				pcmNO.setFormat("Sonic CD Menu Font Regular", 23, FlxColor.fromRGB(254, 174, 0));
-				pcmNO.setBorderStyle(SHADOW, FlxColor.fromRGB(253, 36, 3), 4, 1);
-		
-				daNO.setFormat("Sonic CD Menu Font Regular", 23, FlxColor.fromRGB(174, 179, 251));
-				daNO.setBorderStyle(SHADOW, FlxColor.fromRGB(106, 110, 159), 4, 1);
-			}
-			else
-			{
-				pcmNO.setFormat("Sonic CD Menu Font Regular", 23, FlxColor.fromRGB(174, 179, 251));
-				pcmNO.setBorderStyle(SHADOW, FlxColor.fromRGB(106, 110, 159), 4, 1);
-	
-				daNO.setFormat("Sonic CD Menu Font Regular", 23, FlxColor.fromRGB(254, 174, 0));
-				daNO.setBorderStyle(SHADOW, FlxColor.fromRGB(253, 36, 3), 4, 1);
-			}
-			
-			if (pcmValue < 10)	pcmNO_NUMBER.text = '0' + Std.string(pcmValue);
-			else pcmNO_NUMBER.text = Std.string(pcmValue);
-
-			if (daValue < 10)	daNO_NUMBER.text = '0' + Std.string(daValue);
-			else daNO_NUMBER.text = Std.string(daValue);
-
-					
-
-					
-
-
-
-
-			super.update(elapsed);
-		}
-	
-
+		if (hscriptStates.get('mainmenu').variables.exists('beatHit'))
+			callAllHScript('beatHit', [curBeat]);
+	}
 }
